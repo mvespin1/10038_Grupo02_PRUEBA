7c26dc9a04e3e1c3052377019a1714e4d6a85ced
Ivonne Alexandra Jacome Sangoluisa <iajacome2@espe.edu.ec>
2023-06-25 05:11:23 -0500

    sintaxis

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-//EJEMPLO DE GET STARTED CON MICROSOFT
+//EJEMPLO DE GET STARTED 
 $(document).ready(function() {
     var imagenes = ['./assets/img/imagen1.jpg', './assets/img/imagen2.jpg', './assets/img/imagen3.jpg', './assets/img/imagen4.jpg'];
     var indice = 0;
@@ -9,5 +9,33 @@ $(document).ready(function() {
       $('#imagen').attr('src', nuevaImagen);
     });
 });
+//EJEMPLO DE Syntax
+$(document).ready(function() {
+    var changeColor = true;
+    var defaultColor = $('body').css('background-color');
 
+    function randomColor() {
+      return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
+
+    function changeBackgroundColor() {
+      if (changeColor) {
+        var color = randomColor();
+        $('body').css('background-color', color);
+      }
+    }
+
+    $('.theme-button').click(function() {
+      changeBackgroundColor();
+    });
+
+    $('.disable-button').click(function() {
+      changeColor = false;
+    });
+
+    $('.restore-button').click(function() {
+      changeColor = true;
+      $('body').css('background-color', defaultColor);
+    });
+  });
 
